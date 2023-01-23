098218b9aa02ace7967af695d3f8eafb0cf9934f
barduck <sebastiancamara@gmail.com>
2023-01-23 19:39:47 -0300

    Deleted unused files

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,10 +6,6 @@ const roboto = Roboto_Mono({ weights: ['500', '600', '700'], subsets: ['latin'] 
 export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
-			{/*
-        <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
 			<head />
 			<body className={roboto.className}>{children}</body>
 		</html>
